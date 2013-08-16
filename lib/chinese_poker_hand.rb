1ed283a3db4e66aa16437ff7745960222fef054a
Clayton Lengel-Zigich <clayton@claytonlz.com>
2013-08-16 12:56:33 -0700

    delete it all

--- lib/chinese_poker_hand.rb
+++
@@ -1,9 +0,0 @@
-class ChinesePokerHand
-  def initialize(hand=[], hand_scorer=HandScorer.new)
-    @hand_scorer = hand_scorer
-  end
-
-  def score_against(other_hand)
-    @hand_scorer.score(self, other_hand)
-  end
-end
